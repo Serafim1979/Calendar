7757f2a0d1ab18ddaa698ca82c9d75aaa888cbb5
Ivan Korolkov <korolkov.ivan@mail.ru>
2025-01-01 02:21:29 +0300

    Created a basic window template

--- main.c
+++ main.c
@@ -1,7 +1,81 @@
-#include <stdio.h>
+#include <windows.h>
 
-int main(void)
+// Global constants for window size
+const int WINDOW_WIDTH = 800;
+const int WINDOW_HEIGHT = 600;
+
+// Window Procedure Declaration
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-    printf("Successfully.\n");
+    const char CLASS_NAME[] = "CalendarWindow";
+    WNDCLASS wc = {};
+
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+
+    RegisterClass(&wc);
+
+    // Create the window
+    HWND hwnd = CreateWindowEx(
+        0,
+        CLASS_NAME,
+        "Calendar",
+        WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU,
+        CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
+        NULL, NULL, hInstance, NULL
+    );
+
+    if(hwnd == NULL)
+    {
+        return 0;
+    }
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = {};
+    while(GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch(uMsg)
+    {
+    case WM_CLOSE:
+        {
+            DestroyWindow(hwnd);
+        }
+        break;
+
+    case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+        }
+        break;
+
+    case WM_GETMINMAXINFO:
+        {
+            MINMAXINFO* mmi = (MINMAXINFO*)lParam;
+            mmi->ptMinTrackSize.x = WINDOW_WIDTH;
+            mmi->ptMinTrackSize.y = WINDOW_HEIGHT;
+            mmi->ptMinTrackSize.x = WINDOW_WIDTH;
+            mmi->ptMinTrackSize.y = WINDOW_HEIGHT;
+            return 0;
+        }
+
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
     return 0;
 }
